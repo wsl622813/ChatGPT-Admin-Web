d37b45c52ce1a84c73dadcd5d2550116eba97511
emegahoude <emegahoude@gmail.com>
2023-07-25 11:10:41 +0800

    fix limits

--- apps/chat/app/api/bots/[model]/route.ts
+++ apps/chat/app/api/bots/[model]/route.ts
@@ -45,16 +45,18 @@ export const POST = serverErrorCatcher(
         );
     }
 
-    //使用resetChances字段作为plan, 0为普通用户, 1为付费用户
-    const user = await UserDAL.getUser(userId);
-    console.log("plan:", user.resetChances)
-
     //默认免费用户三分钟内2次, 付费用户三分钟内20次
     let limitNum = 2;
     let duration = 10800;
-    if (user.resetChances === 1) {
+
+    //使用resetChances字段作为plan, 0为普通用户, 1为付费用户
+    const user = await UserDAL.getUser(userId);
+    
+    if (user != null && user.resetChances === 1) {
+      console.log("plan:", user.resetChances)
       limitNum = 20;
     }
+
     console.log("limitNum:", limitNum)
 
     if (!(await rateLimit(userId.toString(), model, limitNum, duration)))
